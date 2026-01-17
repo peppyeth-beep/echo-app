c918ebb0cca0c08a8c225374c0a341d18b661093
peppyeth-beep <peppyeth@gmail.com>
2026-01-17 12:10:39 +0530

    Fixed socket URL for production

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const URL = 'http://localhost:4001';
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:4000';
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
